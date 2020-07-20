bbbe7e9f043de88f6d2b7ea12c46eb4118604026
Carlos Murillo <chaccool@hotmail.com>
2020-07-19 23:12:23 -0500

    Xamarin Inteligente - Module 2

---
+++ XamarinInteligente/XamarinInteligente/XamarinInteligente/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinInteligente.Views;
+
+namespace XamarinInteligente
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ClientSearchPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
